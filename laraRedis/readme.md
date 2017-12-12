fbe392fe178dd9ad4d82c6af95bdfa4e50e0e9c9
ambuilding <explorewo@gmail.com>
2017-12-12 16:33:29 +0800

    Redis / Cache drive

---
+++ laraRedis/readme.md
@@ -0,0 +1,5 @@
+## Real-time Laravel with Socket.io and Redis
+
+- Redis / Cache drive
+- Publish events, and catch them from our Node server.
+
--- laraRedis/routes/web.php
+++ laraRedis/routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Redis;
 
 Route::get('/', function () {
-    return view('welcome');
+	Redis::set('name', 'Dniya');
+
+	//return Redis::get('name');
+    //return view('welcome');
+    return Redis::hget('preferences', 'length');
+});
+
+Route::get('/cache', function () {
+	Cache::put('foo', 'bar', 10);
+
+	return Cache::get('foo');
 });
